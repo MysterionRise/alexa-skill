3175d9defc6a3f1bab2af4e19c626373607fcfae
Mysterion <pericov.ko@gmail.com>
2014-06-08 12:17:19 +0400

    exception catch

--- src/Main.hs
+++ src/Main.hs
@@ -1,8 +1,18 @@
 import Data.Text
 import Helpers
+import Control.Exception
+
+tryToOpenFile :: FilePath -> IO String
+tryToOpenFile path =
+     readFile path `catch` errors
+     where
+         errors :: IOException -> IO String
+         errors error = return $ show error
 
 main :: IO()
 main = do
-     putStrLn "Input your text, please"
+     putStrLn "Input path to your text file, please"
      lineFromUser <- getLine
      putStrLn $ "We've got: " ++ lineFromUser
+     fileContent <- tryToOpenFile lineFromUser
+     putStrLn fileContent
