ba8972a50d83968e6d6011f22f698251731749a0
Mysterion <pericov.ko@gmail.com>
2014-06-21 11:30:39 +0400

    add more stuff

--- src/Main.hs
+++ src/Main.hs
@@ -2,9 +2,10 @@ import Helpers
 
 main :: IO()
 main = do
-    putStrLn $ hello getCurrentUser
+    greetUser
+    putStrLn ""
     putStrLn "---------------------------------------------"
     putStrLn "Enter your command, please: "
     input <- getLine
     if input == "start" then startShell else exitShell
-    
+
--- src/Utils/Helpers.hs
+++ src/Utils/Helpers.hs
@@ -1,25 +1,27 @@
 module Helpers where
 
-import System.Process 
+import System.Process
 import System.Directory
+import System.Posix.User
 import Category
 
-hello user = "Hi, " ++ user
-
-getCurrentUser = undefined
+-- not portable, use posix stuff
+greetUser = do
+        user <- getRealUserID >>= getUserEntryForID
+        putStrLn $ "Hi, " ++ (userName user)
 
 runSysCmd = system
 
-startShell = do	
-	putStrLn "Octo Shell starting..."	
-	currentDir <- getCurrentDirectory
-	categorizeFolder currentDir
-	-- more to come
-	exitShell
+startShell = do
+        putStrLn "Octo Shell starting..."
+        currentDir <- getCurrentDirectory
+        categorizeFolder currentDir
+        -- more to come
+        exitShell
 
 exitShell = do
-	putStrLn "Octo Shell exiting..."
-	-- more to come
+          putStrLn "Octo Shell exiting..."
+          -- more to come
 
 categorizeFolder :: FilePath -> IO [Category]
 categorizeFolder dir = undefined
